c2ce9678bb2a5858f3875d9c95114182fa4e17ab
Allen Garvey <allengarvey@hotmail.com>
2018-02-08 03:18:40 +0000

    Add functionality to add new expense

--- lib/mundum_web/controllers/expense_controller.ex
+++ lib/mundum_web/controllers/expense_controller.ex
@@ -4,24 +4,51 @@ defmodule MundumWeb.ExpenseController do
   alias Mundum.Editor
   alias Mundum.Editor.Expense
 
+  def custom_render(conn, template, assigns) do
+    custom_render(conn, view_module(conn), template, assigns)
+  end
+
+  def custom_render(conn, view_module, template, assigns) do
+    assigns = [{:item_name_singular, "expense"}] ++ assigns
+    render(conn, view_module, template, assigns)
+  end
+
   def index(conn, _params) do
     expenses = Editor.list_expenses()
     render(conn, "index.html", expenses: expenses)
   end
 
-  def new(conn, _params) do
-    changeset = Editor.change_expense(%Expense{})
-    render(conn, "new.html", changeset: changeset)
+  def related_fields do
+    categories = Editor.list_categories() |> MundumWeb.CategoryView.map_for_form
+    [categories: categories]
   end
 
-  def create(conn, %{"expense" => expense_params}) do
+  def new_page(conn, changeset, _params) do
+    custom_render(conn, "new.html", changeset: changeset, related_fields: related_fields())
+  end
+
+  def new(conn, params) do
+    changeset = Editor.change_expense(%Expense{})
+    new_page(conn, changeset, params)
+  end
+
+  def create_succeeded(conn, expense, "true") do
+    changeset = Editor.change_quote(%Expense{date_incurred: expense.date_incurred, category_id: expense.category_id})
+    new_page(conn, changeset, nil)
+  end
+
+  def create_succeeded(conn, expense, _save_another) do
+    redirect(conn, to: expense_path(conn, :show, expense))
+  end
+
+  def create(conn, %{"expense" => expense_params, "save_another" => save_another}) do
     case Editor.create_expense(expense_params) do
       {:ok, expense} ->
         conn
         |> put_flash(:info, "Expense created successfully.")
-        |> redirect(to: expense_path(conn, :show, expense))
+        |> create_succeeded(expense, save_another)
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        new_page(conn, changeset, nil)
     end
   end
 
