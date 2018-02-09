1fe107920aa3161301bea546efe7c956a6c6deb8
Allen Garvey <allengarvey@hotmail.com>
2018-02-09 21:43:05 +0000

    Create function to render delete button

--- lib/mundum_web/views/shared_view.ex
+++ lib/mundum_web/views/shared_view.ex
@@ -38,4 +38,22 @@ defmodule MundumWeb.SharedView do
         
         render "form_page.html", assigns
     end
+    
+    @doc """
+    Displays button with hidden delete form
+    required keywords in assigns are: :item_name_singular, :conn, :item
+    optional keywords in assigns are: :button_class
+    """
+    def render("delete_button.html", assigns) do
+        delete_path = path_for_item(assigns[:conn], assigns[:item_name_singular], :delete, assigns[:item])
+        button_class = case assigns[:button_class] do
+            nil -> "btn btn-danger btn-xs"
+            button_class -> button_class
+        end
+        form_for(assigns[:conn], delete_path, [method: "delete", data_form: "delete", class: "delete-form"], fn _f ->
+            ~e"""
+            <%= submit "Delete", class: button_class %>
+            """
+        end)
+    end
 end
